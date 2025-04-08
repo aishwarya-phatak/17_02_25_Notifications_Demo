87924487e6402a3eb67762abfbd19c6ea70308b2
aishwarya <ashp17star@gmail.com>
2025-04-08 11:43:48 +0530

    Notifications - InboxStyle, BigPictureStyle, Simple Notification

--- app/src/main/java/com/bitcode/a17_02_25_notifications_demo/MainActivity.kt
+++ app/src/main/java/com/bitcode/a17_02_25_notifications_demo/MainActivity.kt
@@ -5,12 +5,15 @@ import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.BigPictureStyle
+import androidx.core.app.NotificationCompat.InboxStyle
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.PendingIntentCompat
 import androidx.core.view.ViewCompat
@@ -22,6 +25,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManagerCompat
     private val bitcodeChannelId = "BitcodeNotificationChannel"
     private val SIMPLE_NOTIFICATION = 1
+    private val BIG_PICTURE_NOTIFICATION = 2
+    private val ACTION_TEXT_STYLE_NOTIFICATION = 3
+    private val INBOX_STYLE_NOTIFICATION = 4
+    private val SNACK_BAR_NOTIFICATION = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,12 +72,51 @@ class MainActivity : AppCompatActivity() {
         notificationManager.notify(SIMPLE_NOTIFICATION, notificationCompat.build())
     }
 
+    @SuppressLint("MissingPermission", "NewApi")
     private fun bigPictureStyleNotification(){
+        var notificationCompat = NotificationCompat.Builder(this,bitcodeChannelId)
 
+        var bigPictureStyle = BigPictureStyle(notificationCompat)
+
+        var bitmapImage = BitmapFactory.decodeResource(resources,R.drawable.test_image_3)
+
+        notificationCompat.setSmallIcon(R.drawable.ic_launcher_background)
+
+        bigPictureStyle.bigLargeIcon(bitmapImage)
+        bigPictureStyle.bigPicture(bitmapImage)
+        bigPictureStyle.setBigContentTitle("Upcoming Batch Schedule")
+        bigPictureStyle.setSummaryText("Android, iOS, Web batches will be starting in April 2025 2nd Week")
+        bigPictureStyle.setContentDescription("Android, iOS, Web batches will be starting in April 2025 2nd Week")
+        bigPictureStyle.build()
+
+        notificationManager.notify(BIG_PICTURE_NOTIFICATION,notificationCompat.build())
     }
 
+    @SuppressLint("MissingPermission")
     private fun inboxStyleNotification(){
 
+        var notificationCompat = NotificationCompat.Builder(this,bitcodeChannelId)
+        notificationCompat.setSmallIcon(R.drawable.test_image_3)
+        var inboxStyle = InboxStyle(notificationCompat)
+
+        inboxStyle.setSummaryText("Upcoming Bitcode Batches For April")
+        inboxStyle.setBigContentTitle("Batch Schedule 2025")
+        inboxStyle.addLine("C/Cpp batch by Snehal Mam")
+        inboxStyle.addLine("iOS Batch by Aishwarya Mam")
+        inboxStyle.addLine("Android Batch by Aishwarya Mam")
+        inboxStyle.addLine("Web Batch By Akanksha Mam")
+        inboxStyle.addLine("Core Java Batch By Vishal Sir")
+
+        var intent = Intent(this,DetailsActivity::class.java)
+
+        var pendingIntent = PendingIntent.getActivity(this,
+            1,
+            intent,
+            PendingIntent.FLAG_MUTABLE)
+        notificationCompat.setContentIntent(pendingIntent)
+
+        inboxStyle.build()
+        notificationManager.notify(INBOX_STYLE_NOTIFICATION,notificationCompat.build())
     }
 
     private fun createNotificationChannel(){
